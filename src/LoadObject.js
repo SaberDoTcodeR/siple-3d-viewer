95bad0e26bd59aebab18d3ccd6580853c8cf91fa
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2023-04-25 22:54:59 +0100

    feat(Quad WireFrame): Supports 3d obj with quad

--- src/LoadObject.js
+++ src/LoadObject.js
@@ -4,48 +4,70 @@ import { useEffect, useState } from "react";
 import * as THREE from "three";
 import { useLoader, useThree } from "@react-three/fiber";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import modelPath from "./models/Cube.obj";
-import "./App.css";
+import CubePath from "./models/Cube.obj";
+import CubeHalfQuadPath from "./models/Cube Half Triangles.obj";
+import AnimalPath from "./models/animal.obj";
+import BagPath from "./models/bag.obj";
 
+import "./App.css";
+import OBJFile from "obj-file-parser";
+import {QuadWireFrameGeometry} from "./quadWireFrameGeometry";
+const Paths = [CubePath, CubeHalfQuadPath, AnimalPath, BagPath]
 export default function LoadObject() {
   const [childMaterials, setChildMaterials] = useState({});
   const { scene } = useThree();
-
-  const object = useLoader(OBJLoader, modelPath);
-
+  const objects = useLoader(OBJLoader, Paths);
+  const objVertices = async (modelPath) =>{
+    return fetch(modelPath)
+      .then((r) => r.text())
+      .then(objText  => {
+        const objFile = new OBJFile(objText);
+        return objFile.parse()
+      })
+  }
   useEffect(() => {
     let mounted = true;
     if (mounted) {
       const materialsChildren = {};
-      object.traverse((child) => {
-        if (child.isMesh) {
-          materialsChildren[child.uuid] = child.material;
-        }
-      });
+      objects.forEach((obj)=>{
+        obj.traverse((child) => {
+          if (child.isMesh) {
+            materialsChildren[child.uuid] = child.material;
+          }
+        });
+      })
+
       setChildMaterials(materialsChildren);
     }
     return () => {
       mounted = false;
     };
-  }, [object]);
+  }, [objects]);
 
   useEffect(() => {
     let mounted = true;
     if (mounted && childMaterials && Object.keys(childMaterials).length > 0) {
-      object.traverse((child) => {
-        if (child.isMesh) {
-          child.castShadow = true;
-          child.receiveShadow = true;
-          if (childMaterials[child.uuid]) {
-            child.material = new THREE.MeshPhongMaterial({
-              color: child.material,
-              wireframe: false,
-            });
+      objects.forEach((obj, index)=>{
+        obj.traverse((child) => {
+          if (child.isMesh) {
+            child.visible = false
+            if (childMaterials[child.uuid]) {
 
-            scene.add(child);
+              objVertices(Paths[index]).then((object) => {
+                const wireframe = new QuadWireFrameGeometry(child.geometry, object.models[0]);
+                const line = new THREE.LineSegments( wireframe );
+                line.material.depthTest = false;
+                line.material.opacity = 1;
+                line.material.transparent = true;
+                line.material.color = new THREE.Color( 0xff0000 );
+                line.position.set(index * 10, 0, 0);
+                scene.add( line );
+              })
+              scene.add(child);
+            }
           }
-        }
-      });
+        });
+      })
     }
     return () => {
       mounted = false;
